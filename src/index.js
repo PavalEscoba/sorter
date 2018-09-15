12d4555882bcf0112dfdf02519bfab41f735464c
Paval Miatlitski <paval.escoba@gmail.com>
2018-09-15 18:00:38 +0300

    third task solved

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.storage = [];
+    this.averageSort = (a, b) => a - b;
   }
 
   add(element) {
     // your implementation
+    this.storage.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.storage[index];
   }
 
   get length() {
     // your implementation
+    return this.storage.length;
   }
 
   toArray() {
     // your implementation
+    return this.storage;
   }
 
   sort(indices) {
     // your implementation
+    indices.sort();
+    const tempArr = [];
+    for(let i = 0; i<indices.length; i++){
+      tempArr.push(this.storage[indices[i]]);
+    }
+    tempArr.sort(this.averageSort);
+    for( let i=0; i<indices.length; i++){
+      this.storage[indices[i]] = tempArr[i];
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.averageSort = compareFunction;
   }
 }
 
